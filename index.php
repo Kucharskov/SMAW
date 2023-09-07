89040a0f64e7a845437e5ffcd004d78719d2ad35
Michał Kucharski <M.Kucharskov@onet.pl>
2023-09-07 19:02:37 +0200

    Refactor 3.0 - Readded id Hashing
    Source: https://github.com/dmhendricks/hash-int/
    Co-Authored-By: Daniel M. Hendricks <8184361+dmhendricks@users.noreply.github.com>

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $SMAW_CONFIG["BaseFile"]	= "SMAWurls.txt";				// Name of your links file databas
 $SMAW_CONFIG["ShowLast"]	= 5;							// Show last X shortened URLs (0 - Disabled, X - Enabled to show X last)
 $SMAW_CONFIG["LinksCount"]	= 1;							// Show link counter with all shortened links (0 - Disabled, 1 - Enabled)
 $SMAW_CONFIG["RewriteMod"]	= 1;							// Use mod_rewrite links "domain.com/X" instead of "domain.com/index.php?id=X" (0 - Disabled, 1 - Enabled)
-$SMAW_CONFIG["HashLinks"]	= 0;							// Use base64 function to hash link ID (0 - Disabled, 1 - Enabled)
+$SMAW_CONFIG["HashLinks"]	= 1;							// Use base64 function to hash link ID (0 - Disabled, 1 - Enabled)
 $SMAW_CONFIG["FixSlash"]	= 0;							// Fixed doubled slash // for some apache/nginx servers (0 - Disabled, 1 - Enabled)
 
 // Translations of SMAW
@@ -55,6 +55,92 @@ $SMAW_TRANS["pl"] = array(
 	"BaseProblem"	=> "Plik z bazą nie istnieje lub nie ma danych poprawnych praw CHMOD (777)"
 );
 
+// Whole class for generating proper hashes for specific IDs
+// Source: https://github.com/dmhendricks/hash-int/
+/**
+* Generate a short hash from an integer
+* @link http://web.archive.org/web/20130727034425/http://blog.kevburnsjr.com/php-unique-hash
+*/
+class HashInt {
+	/* Key: Next prime greater than 62 ^ n / 1.618033988749894848 */
+	/* Value: modular multiplicative inverse */
+	private static $golden_primes = array(
+		'1'                  => '1',
+		'41'                 => '59',
+		'2377'               => '1677',
+		'147299'             => '187507',
+		'9132313'            => '5952585',
+		'566201239'          => '643566407',
+		'35104476161'        => '22071637057',
+		'2176477521929'      => '294289236153',
+		'134941606358731'    => '88879354792675',
+		'8366379594239857'   => '7275288500431249',
+		'518715534842869223' => '280042546585394647'
+	);
+
+	private static $chars62 = array(
+		0=>48,1=>49,2=>50,3=>51,4=>52,5=>53,6=>54,7=>55,8=>56,9=>57,10=>65,
+		11=>66,12=>67,13=>68,14=>69,15=>70,16=>71,17=>72,18=>73,19=>74,20=>75,
+		21=>76,22=>77,23=>78,24=>79,25=>80,26=>81,27=>82,28=>83,29=>84,30=>85,
+		31=>86,32=>87,33=>88,34=>89,35=>90,36=>97,37=>98,38=>99,39=>100,40=>101,
+		41=>102,42=>103,43=>104,44=>105,45=>106,46=>107,47=>108,48=>109,49=>110,
+		50=>111,51=>112,52=>113,53=>114,54=>115,55=>116,56=>117,57=>118,58=>119,
+		59=>120,60=>121,61=>122
+	);
+
+	/**
+	* Create a hash of defined length from integer
+	*
+	* @param int $num Integer to hash
+	* @param int $len Length of hash to return
+	* @return string
+	*/
+	public static function hash( $num, $len = 6 ) {
+		$ceil = bcpow( 62, $len );
+		$primes = array_keys( self::$golden_primes );
+		$prime = $primes[$len];
+		$dec = bcmod( bcmul( $num, $prime ), $ceil );
+		$hash = self::base62( $dec );
+		return str_pad( $hash, $len, '0', STR_PAD_LEFT );
+	}
+
+	/**
+	* Unhash a hashed string
+	*
+	* @param string $num Integer to hash
+	* @param int $len Length of hash to return
+	* @return string
+	*/
+	public static function unhash( $hash ) {
+		$len = strlen( $hash );
+		$ceil = bcpow( 62, $len );
+		$mmiprimes = array_values( self::$golden_primes );
+		$mmi = $mmiprimes[$len];
+		$num = self::unbase62( $hash );
+		$dec = bcmod( bcmul( $num, $mmi ), $ceil );
+		return $dec;
+	}
+
+	public static function base62( $int ) {
+		$key = '';
+		while( bccomp( $int, 0 ) > 0 ) {
+			$mod = bcmod( $int, 62 );
+			$key .= chr( self::$chars62[$mod] );
+			$int = bcdiv( $int, 62 );
+		}
+		return strrev( $key );
+	}
+
+	public static function unbase62( $key ) {
+		$int = 0;
+		foreach( str_split( strrev( $key ) ) as $i => $char ) {
+			$dec = array_search( ord( $char ), self::$chars62 );
+			$int = bcadd( bcmul( $dec, bcpow( 62, $i ) ), $int );
+		}
+		return $int;
+	}
+}
+
 // Function to display text with multilang text security
 function ShowText($string) {
 	global $SMAW_CONFIG;
@@ -74,6 +160,8 @@ function GetPageTitle($url){
 // Function for getting URL for specific ID
 function GetURL($id) {
 	global $SMAW_CONFIG;
+
+	if($SMAW_CONFIG["HashLinks"]) $id = HashInt::unhash($id);
 
 	if($id < 0) return false;
 	$urls = file($SMAW_CONFIG["BaseFile"]);
@@ -112,6 +200,23 @@ function SaveURL($url) {
 	
 	file_put_contents($SMAW_CONFIG["BaseFile"], "{$url}\r\n", FILE_APPEND);
 	return count($urls) + 1; // Offset for first link
+}
+
+// Function for generating URL with all tweaks
+function GenerateURL($id) {
+	global $SMAW_CONFIG;
+	$basename = pathinfo(__FILE__, PATHINFO_BASENAME);
+	
+	$url = "http";
+	$url .= empty($_SERVER['HTTPS']) ? "":"s";	
+	$url .= "://{$_SERVER["HTTP_HOST"]}";
+	$url .= str_replace($basename, "", $_SERVER["REQUEST_URI"]);
+	if($SMAW_CONFIG["FixSlash"]) $url .= "/";
+	if(!$SMAW_CONFIG["RewriteMod"]) $url .= "{$basename}?id=";
+	if($SMAW_CONFIG["HashLinks"]) $url .= HashInt::hash($id, 7);
+	else $url .= $id;
+	
+	return $url;
 }
 ?>
 <!DOCTYPE html>
@@ -181,8 +286,7 @@ function SaveURL($url) {
 					if(!$status) {
 						echo "<li class='price alert'>".ShowText("BadURL")."</li>\n";
 					} else {
-						$temp_URL = $status; // REFACTOR - Generating full URL for ID
-						echo "<li class='price success'>".ShowText("ShortenURL")."<a href='{$temp_URL}'>{$temp_URL}</a></li>\n";
+						echo "<li class='price success'>".ShowText("ShortenURL")."<a href='".GenerateURL($status)."'>{$temp_URL}</a></li>\n";
 					}
 				} 
 				
@@ -215,10 +319,7 @@ function SaveURL($url) {
 				if(count($last) === 0) echo "<li class='price alert'>".ShowText("NoLastURLs")."</li>\n";
 				foreach($last as $index => $value) {
 					if($value === "") echo "<li class='bullet-item'><span class='alert label'>".ShowText("DeletedURL")."</span></li>\n";
-					else {
-						$temp_URL = $index; // REFACTOR - Generating full URL for ID
-						echo "<li class='bullet-item overflowfix'><a href='{$temp_URL}'>{$value}</a></li>\n";
-					}
+					else echo "<li class='bullet-item overflowfix'><a href='".GenerateURL($index)."'>{$value}</a></li>\n";
 				}
 			?>
 		</ul>
